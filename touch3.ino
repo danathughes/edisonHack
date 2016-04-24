ae7397e4296beeab64f0730ff7ebbb9c0e90b16f
Mike <nordic.savoie@gmail.com>
2016-04-24 07:30:17 -0600

    Added files via upload

---
+++ touch3.ino
@@ -0,0 +1,18 @@
+const int TouchPin=8;
+const int ledPin=4;
+void setup() {
+pinMode(TouchPin, INPUT);
+pinMode(ledPin,OUTPUT);
+} 
+ 
+void loop() {
+int sensorValue = digitalRead(TouchPin);
+if(sensorValue==1)
+{
+digitalWrite(ledPin,HIGH);
+}
+else
+{
+digitalWrite(ledPin,LOW);
+}
+}
